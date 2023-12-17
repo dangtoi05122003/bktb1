20b299bbc5c04dc7cb0ea8deb23d1cd245098fae
dangtoi05122003 <dangtoi05122003@gmail.com>
2023-12-17 23:25:29 +0700

    BTKT

---
+++ domestic/search.php
@@ -0,0 +1,106 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+// Thay đổi thông tin kết nối tới cơ sở dữ liệu của bạn
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tour";
+
+// Tạo kết nối đến cơ sở dữ liệu
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+
+// Kiểm tra xem người dùng đã gửi dữ liệu tìm kiếm hay chưa
+if (isset($_GET['keyword'])) {
+    $search = $_GET['keyword'];
+
+
+    $sql = "SELECT name, price, destination FROM search WHERE name LIKE '%$search%'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0) {
+        echo "Không tìm thấy kết quả.";
+        exit;
+    }
+
+    // Lấy kết quả từ câu truy vấn
+    while ($row = $result->fetch_assoc()) {
+        $name = $row['name'];
+
+        session_start();
+        $_SESSION['name'] = $name;
+
+        // Chuyển hướng đến trang tương ứng
+        switch ($name) {
+            case "America":
+                header("Location: http://localhost/BTKT/dosmetic/america.php");
+                exit;
+            case "France":
+                header("Location: http://localhost/BTKT/dosmetic/france.php");
+                exit;
+            case "Italy":
+                header("Location: http://localhost/BTKT/dosmetic/italy.php");
+                exit;
+            case "Japan":
+                header("Location: http://localhost/BTKT/dosmetic/japan.php");
+                exit;
+            case "England":
+                header("Location: http://localhost/BTKT/dosmetic/england.php");
+                exit;
+            case "Thái Lan":
+                header("Location: http://localhost/BTKT/dosmetic/thailan.php");
+                exit;
+            case "SaudiArabia":
+                header("Location: http://localhost/BTKT/dosmetic/SauArabia.php");
+                exit;
+            case "Canada":
+                header("Location: http://localhost/BTKT/dosmetic/canada.php");
+                exit;
+            case "Israel":
+                header("Location: http://localhost/BTKT/dosmetic/israel.php");
+                exit;
+            case "Spain":
+                header("Location: http://localhost/BTKT/dosmetic/spain.php");
+                exit;
+            case "Hà Nội":
+                header("Location: HaNoi.php");
+                exit;
+            case "Đà Lạt":
+                header("Location: DaLat.php");
+                exit;
+            case "Đà Nẵng":
+                header("Location: DaNang.php");
+                exit;
+            case "Hà Giang":
+                header("Location: HaGiang.php");
+                exit;
+            case "Hạ Long Bay":
+                header("Location: HaLongBay.php");
+                exit;
+            case "Huế":
+                header("Location: Hue.php");
+                exit;
+            case "Mỹ Tho":
+                header("Location: MyTho_CanTho.php");
+                exit;
+            case "Phú Quốc":
+                header("Location: PhuQuoc.php");
+                exit;
+            case "Sapa":
+                header("Location: Sapa.php");
+                exit;
+            case "TP Hồ Chí Minh":
+                header("Location: TPHoChiMinh.php");
+                exit;
+            default:
+                echo "Không tìm thấy trang tương ứng.";
+                exit;
+        }
+    }
+}
+?>
